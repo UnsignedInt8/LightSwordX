e6e722b9881c49c2869f108e07dd4d58d6aa5ad1
SunshinyNeko <sunshiny.neko@gmail.com>
2015-12-17 15:20:38 +0800

    Initial Commit

---
+++ LightSwordX/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  LightSwordX
+//
+//  Created by Neko on 12/17/15.
+//  Copyright © 2015 Neko. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ LightSwordX/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  LightSwordX
+//
+//  Created by Neko on 12/17/15.
+//  Copyright © 2015 Neko. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+

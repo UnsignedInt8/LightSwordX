e4557316c405000ba9e942694e367261c284419c
猫王子 <sunshiny.neko@gmail.com>
2015-12-28 18:15:31 +0800

    Add target for launching helper

---
+++ LightSwordXHelper/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  LightSwordXHelper
+//
+//  Created by Neko on 12/28/15.
+//  Copyright © 2015 Neko. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
